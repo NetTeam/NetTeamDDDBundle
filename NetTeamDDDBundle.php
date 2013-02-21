ad2bc5800b8558dbca6088299b8f3696f19df75c
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2013-02-21 19:46:40 +0100

    Add compiler pass loading orm and odm abstract repositories.

--- NetTeamDDDBundle.php
+++ NetTeamDDDBundle.php
@@ -5,13 +5,18 @@ namespace NetTeam\Bundle\DDDBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use NetTeam\Bundle\DDDBundle\DependencyInjection\DDDExtension;
+use NetTeam\Bundle\DDDBundle\DependencyInjection\Compiler\DoctrineRepositoryPass;
 
 class NetTeamDDDBundle extends Bundle
 {
-     public function build(ContainerBuilder $container)
-     {
-         parent::build($container);
 
-         $container->registerExtension(new DDDExtension());
-     }
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->registerExtension(new DDDExtension());
+
+        $container->addCompilerPass(new DoctrineRepositoryPass());
+    }
+
 }
