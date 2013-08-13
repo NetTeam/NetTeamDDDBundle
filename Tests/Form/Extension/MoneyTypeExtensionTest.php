bf6464b71e2ae97f8789fa7399fc60777150370f
Krzysztof Menżyk <krzysztof.menzyk@netteam.pl>
2013-08-13 20:08:49 +0200

    Testy dla type extensionów

--- Tests/Form/Extension/MoneyTypeExtensionTest.php
+++ Tests/Form/Extension/MoneyTypeExtensionTest.php
@@ -2,40 +2,42 @@
 
 namespace NetTeam\Bundle\DDDBundle\Tests\Form\Extension;
 
-use NetTeam\DDD\ValueObject\Money;
 use NetTeam\Bundle\DDDBundle\Form\Extension\MoneyTypeExtension;
-use Symfony\Component\Form\Test\TypeTestCase;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use NetTeam\DDD\ValueObject\Money;
 
 /**
- * @author Paweł A. Wacławczyk <p.a.waclawczyk@gmail.com>
+ * @author Krzysztof Menżyk <krzysztof.menzyk@netteam.pl>
+ *
+ * @group Unit
  */
-class MoneyTypeExtensionTest extends TypeTestCase
+class MoneyTypeExtensionTest extends TypeExtensionTestCase
 {
-
-    protected function setUp()
+    protected function getTypeExtensions()
     {
-        $this->markTestSkipped();
+        return array(
+            new MoneyTypeExtension(),
+        );
     }
 
-    public function testForm()
+    public function testBindUsingValueObject()
     {
-
-        $type = new MoneyType();
-
-        $form = $this->factory->create($type); //, null, array('use_value_object' => true));
-        $money = new Money(123.45, 'PLN');
-
-        $form->submit(array(123.45));
-
-        $this->assertTrue($form->isSynchronized());
-        $this->assertEquals($money, $form->getData());
-    }
-
-    protected function getExtensions()
-    {
-        return array_merge(parent::getExtensions(), array(
-            new MoneyTypeExtension()
+        $form = $this->factory->create('money', null, array(
+            'use_value_object' => true,
         ));
+
+        $form->bind('123');
+
+        $this->assertEquals(new Money(123, 'EUR'), $form->getData());
+        $this->assertSame(123.0, $form->getNormData());
+    }
+
+    public function testBindUsingDefault()
+    {
+        $form = $this->factory->create('money');
+
+        $form->bind('123');
+
+        $this->assertSame(123.0, $form->getData());
+        $this->assertSame(123.0, $form->getNormData());
     }
 }
