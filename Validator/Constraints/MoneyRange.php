f8b46464e08a7c1a6f2553d6856f603e62bcf397
Paweł A. Wacławczyk <pawel.waclawczyk@netteam.pl>
2013-08-14 14:04:22 +0200

    Dodanie walidatora dla MoneyRange.

---
+++ Validator/Constraints/MoneyRange.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace NetTeam\Bundle\DDDBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Constraint for MoneyRange.
+ *
+ * @author Paweł Wacławczyk <pawel.waclawczyk@netteam.pl>
+ *
+ * @Annotation
+ */
+class MoneyRange extends Constraint
+{
+    public $message = 'moneyRange.invalidValue';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTargets()
+    {
+        return array(self::CLASS_CONSTRAINT, self::PROPERTY_CONSTRAINT);
+    }
+}
